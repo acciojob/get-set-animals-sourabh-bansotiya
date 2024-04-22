b54d94ab9bb3aaecc69e99edfb944710095370d8
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-22 21:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,18 +12,24 @@ class Animal {
 }
 
 class Cat extends Animal {
-	const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
-
+	purr(){
+		console.log("purr");
+	}
 }
 
 class Dog extends Animal {
-	const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
+	bark(){
+		console.log("woof");
+	}
 }
 
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 
 
 // Do not change the code below this line
